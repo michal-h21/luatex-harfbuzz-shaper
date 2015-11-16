acb5db1ebcf8bad05f4f9a34cf6ee3d996c02a52
Michal Hoftich <michal.h21@gmail.com>
2015-11-16 20:48:48 +0100

    Interface to luaotfload font search

---
+++ hb_findfont.lua
@@ -0,0 +1,86 @@
+-- Code copied from luaotfload-tool
+-- we need a minimal interface to luaotfload font database
+--
+config                          = config or { }
+local config                    = config
+config.luaotfload               = config.luaotfload or { }
+
+config.lualibs                  = config.lualibs or { }
+config.lualibs.verbose          = false
+config.lualibs.prefer_merged    = true
+config.lualibs.load_extended    = true
+
+require "lualibs"
+local iosavedata                = io.savedata
+local lfsisdir                  = lfs.isdir
+local lfsisfile                 = lfs.isfile
+local stringsplit               = string.split
+local tableserialize            = table.serialize
+local tablesortedkeys           = table.sortedkeys
+local tabletohash               = table.tohash
+local dummy_function = function ( ) end                                                                                                               
+local backup = {                                                                                                                                      
+  write     = texio.write,                                                                                                                          
+  write_nl  = texio.write_nl,                                                                                                                       
+  utilities = utilities,                                                                                                                            
+}                                                                                                                                                     
+
+texio.write, texio.write_nl          = dummy_function, dummy_function                                                                                 
+require"luaotfload-basics-gen.lua"                                                                                                                    
+
+texio.write, texio.write_nl          = backup.write, backup.write_nl                                                                                  
+utilities                            = backup.utilities                                                                                               
+
+require "luaotfload-log.lua"       --- this populates the luaotfload.log.* namespace                                                                  
+require "luaotfload-parsers"       --- fonts.conf, configuration, and request syntax                                                                  
+require "luaotfload-configuration" --- configuration file handling    
+require "luaotfload-database"
+require "luaotfload-features"
+
+-- load default configuration
+
+config.actions.apply_defaults()
+
+-- local parser = luaotfload.parsers.font_request
+-- local lpeg = lpeg
+-- local lpegmatch = lpeg.match
+
+local resolve_cached = fonts.names.resolve_name
+local handle_request = fonts.names.handle_request
+local resolve_fullpath = fonts.names.getfilename
+
+local function find(specification, size)
+  local request = {features = {}}
+  local specification = specification or ""
+  request.specification = specification
+  local spec = handle_request(request)
+
+  -- for k,v in pairs(fonts.names) do
+  --   print(k,v)
+  -- end
+
+  -- print "-----------------"
+  -- for k,v in pairs(spec) do
+  --   print(k,v)
+  -- end
+
+  local fontfile, a = resolve_cached(spec)
+  if not fontfile then
+    return nil, "Cannot load font " .. specification
+  end
+
+  local base, ext = fontfile:match("(.+)%.([^.]+)")
+  if not base then 
+    return nil, "Probably tfm font"
+  end
+  local fullpath = resolve_fullpath(base,ext)
+  spec.fullpath = fullpath
+  spec.fontfile = fontfile
+  return fullpath, spec
+end
+
+local M = {}
+
+M.find = find
+
+return M
